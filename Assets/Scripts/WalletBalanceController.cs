9da9a05c5592827f4e4e8722aa399ec4435a3041
xactant <xactant@gmail.com>
2021-11-30 08:26:34 -0500

    Add some file descriptors and readme files to code folders.

--- Assets/Scripts/WalletBalanceController.cs
+++ Assets/Scripts/WalletBalanceController.cs
@@ -1,8 +1,43 @@
+/**
+ *           Module: MoralisSessionTokenResponse.cs
+ *  Descriptiontion: Sample script showing how to retrieve user and balance 
+ *                   information using Moralis Web3API and displaying those 
+ *                   items in text elements.
+ *           Author: Moralis Web3 Technology AB, 559307-5988 - David B. Goodrich
+ *  
+ *  MIT License
+ *  
+ *  Copyright (c) 2021 Moralis Web3 Technology AB, 559307-5988
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
 using Moralis.Platform.Objects;
 using Moralis.Web3Api.Models;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Sample script showing how to retrieve user and balance information using 
+/// Moralis Web3API and displaying those items in text elements.
+/// NOTE: This could probably be combined with the TokenListController script since
+///       the information is now combined in a single view.
+/// </summary>
 public class WalletBalanceController : MonoBehaviour
 {
     /// <summary>
@@ -29,20 +64,27 @@ public class WalletBalanceController : MonoBehaviour
 
         if (user != null)
         {
+            // Get user address from user auth data.
             string addr = user.authData["moralisEth"]["id"].ToString();
 
             addressText.text = addr;
 
+            // Retrieve account balanace.
             NativeBalance bal =
                 MoralisInterface.GetClient().Web3Api.Account.GetNativeBalance(addr.ToLower(),
                                             (ChainList)ChainId);
             double balance = 0.0;
             
+            // Make sure a response to the balanace request weas received. The 
+            // IsNullOrWhitespace check may not be necessary ...
             if (bal != null && !string.IsNullOrWhiteSpace(bal.Balance))
             {
                 double.TryParse(bal.Balance, out balance);
             }
 
+            // Display native token amount (ETH) in fractions of ETH.
+            // NOTE: May be better to link this to chain since some tokens may have
+            // more than 18 sigjnificant figures.
             balanceText.text = string.Format("{0:0.#####}", balance / (double)Mathf.Pow(10.0f, 18.0f));
         }
         else
---
+++ Assets/Scripts/readme.md
@@ -0,0 +1,11 @@
+﻿# Moralis Unity Bolierplate Files #
+The files in this directory are provided to deomnstrate how to authetnicate a user 
+using Moralis and the user's wallet and query Web3 data using the Moralis Web3API and
+then display that information in the game. Consider the files in this folder as an example.
+
+## Child Folders ##
+The files in the child folders should not be modified:
+- Moralis
+- WalletConnectSharp
+- WalletConnectSharp.Unity
+
