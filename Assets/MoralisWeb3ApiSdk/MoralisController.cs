0db05b2ff57e2b38b621c979aff55532de6e4790
xactant <xactant@gmail.com>
2022-04-07 07:16:36 -0400

    Remove SERVER URI Warning. Fix manifest entry for Newtonsoft.

--- Assets/MoralisWeb3ApiSdk/MoralisController.cs
+++ Assets/MoralisWeb3ApiSdk/MoralisController.cs
@@ -57,10 +57,7 @@ namespace MoralisWeb3ApiSdk
         public WalletConnect walletConnect;
         private void OnValidate()
         {
-            if (MoralisServerURI == "SERVER URI" || string.IsNullOrWhiteSpace(MoralisServerURI) || MoralisApplicationId == "APPLICATION ID" || string.IsNullOrWhiteSpace(MoralisApplicationId))
-            {
-                Debug.LogError("Setup your Moralis Server URI and Application Id before running. For more help read the Quick Start on: https://github.com/ethereum-boilerplate/ethereum-unity-boilerplate#-quick-start");
-            }
+
         }
 
 #if UNITY_WEBGL
