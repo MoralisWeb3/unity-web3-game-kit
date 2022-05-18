0bee00135e1980b2c5bde27d013254de1f3d777c
Koen Rijpstra <k.rijpstra@gmail.com>
2022-05-18 20:57:59 +0200

    - Added Introduction demo - Added AuthenticationKit prefab as a variant - Cleaned up project .gitignore check

---
+++ Assets/Moralis Web3 Unity SDK/Demos/Introduction/Scripts/AuthenticationController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AuthenticationController : MonoBehaviour
+{
+    public static void ShowMainScene()
+    {
+        //SceneManager.LoadScene(1);
+    }
+}
